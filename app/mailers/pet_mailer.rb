3f7554f5a2b741ece6266e82c75d8fce64aa6b63
rachelkathleen <rachel.hawa@gmail.com>
2019-10-02 13:22:44 -0500

    .gitignore is now working

--- app/mailers/pet_mailer.rb
+++
@@ -1,9 +0,0 @@
-class PetMailer < ApplicationMailer
-
-  def pet_email(pet)
-      @pet = pet
-      mail(to: @pet.recipient_email,
-        cc: @pet.sender_email,
-        subject: "Look at this pet!")
-  end
-end
--- app/models/pet.rb
+++
@@ -1,4 +0,0 @@
-class Pet < ApplicationRecord
-  validates :recipient_name, :recipient_email, :sender_name, :sender_email, presence: true
-
-end
