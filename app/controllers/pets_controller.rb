034cf6fac8906ffb879aef91ed42923d8ca9d886
rachelkathleen <rachel.hawa@gmail.com>
2019-10-02 13:23:20 -0500

    .gitignore is now working

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,30 @@
+class PetsController < ApplicationController
+skip_before_action :verify_authenticity_token
+  def index 
+    pets = Pet.All 
+    render json: pets 
+  end
+
+
+  def new
+    @pet = Pet.new
+  end
+  
+
+  def create
+    # binding.pry
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      PetMailer.pet_email(@pet).deliver_now
+      render json: @pet
+    else
+      render json: {status: 500, message: 'Error creating pet'}
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:recipient_email, :recipient_name, :sender_email, :sender_name, :message, :url, :name, :photo)
+  end
+end
---
+++ db/migrate/20190906040517_create_pets.rb
@@ -0,0 +1,16 @@
+class CreatePets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets do |t|
+      t.string :sender_name
+      t.string :recipient_name
+      t.string :sender_email
+      t.string :recipient_email
+      t.text :message
+      t.string :url
+      t.string :name
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
