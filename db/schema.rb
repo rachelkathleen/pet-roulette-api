3b1aa94dcc5264a9d956b94d8f7aaaea86776f66
rachelkathleen <rachel.hawa@gmail.com>
2019-10-03 21:10:48 -0500

    add species

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_011943) do
+ActiveRecord::Schema.define(version: 2019_10_02_205727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 2019_10_04_011943) do
     t.string "url"
     t.string "name"
     t.string "photo"
+    t.string "species"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "type"
   end
 
 end
