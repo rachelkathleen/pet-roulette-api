f1de7a1d6df06e32308635b13508329781a3b669
rachelkathleen <rachel.hawa@gmail.com>
2019-10-04 00:16:18 -0500

    update subject

--- app/mailers/pet_mailer.rb
+++ app/mailers/pet_mailer.rb
@@ -4,6 +4,6 @@ class PetMailer < ApplicationMailer
       @pet = pet
       mail(to: @pet.recipient_email,
         cc: @pet.sender_email,
-        subject: "Someone sent you a pet from Pet Roulette!")
+        subject: (@pet.sender_name)+" sent you a pet from Pet Roulette!")
   end
 end
