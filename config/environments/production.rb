0dfa1980c04e86f10f03979b18bfaa571fc35bd8
rachelkathleen <rachel.hawa@gmail.com>
2019-10-06 20:49:08 -0500

    add action mailer to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'petrouletteflatiron@gmail.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:  'smtp.gmail.com',
+    port:                 5000,
+    domain:  'gmail.com',
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+ 
 end
