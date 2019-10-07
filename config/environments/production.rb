1f7f47801ce3c9abd35daa98847b237ca3968e86
rachelkathleen <rachel.hawa@gmail.com>
2019-10-06 20:49:44 -0500

    update port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:  'smtp.gmail.com',
-    port:                 5000,
+    port:                 587,
     domain:  'gmail.com',
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
