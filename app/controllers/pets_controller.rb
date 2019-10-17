c136c181b1a9f6aa056d92593990ea2fbf716dcd
rachelkathleen <rachel.hawa@gmail.com>
2019-10-16 23:12:39 -0500

    add edit to controller and routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,9 +23,20 @@ skip_before_action :verify_authenticity_token
     end
   end
 
+  def update
+    binding.pry
+    pet =  Pet.find(params[:id])
+    
+    if pet.update(pet_params)
+        render json: pet
+    else
+        render json: {status: 500, message: 'Pet cannot be updated'}
+    end
+end
+
   private
 
   def pet_params
-    params.require(:pet).permit(:recipient_email, :recipient_name, :sender_email, :sender_name, :message, :url, :name, :photo, :species, :likes)
+    params.require(:pet).permit(:recipient_email, :recipient_name, :sender_email, :sender_name, :message, :url, :name, :photo, :species, :likes, :likes)
   end
 end
